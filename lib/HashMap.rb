6736158fea83513c553fee2333368aaae6513406
NathanD <nathand50444@gmail.com>
2024-08-12 21:11:32 +0100

    'set' method allows us to set the given hash
    Outlined other methods in comments

--- lib/HashMap.rb
+++ lib/HashMap.rb
@@ -7,4 +7,22 @@ class HashMap
 
     hash_code
   end
+
+  def set(key, value)
+    my_hash = {}
+    my_hash[key] = value
+    my_hash
+  end
+
+  def get(key)
+    # This method will search the buckets for the corresponding key; value pair.
+  end
+
+  def has?(key)
+    # Search the buckets for the corresponding key; value pair, return TRUE if found.
+  end
+
+  def remove(key)
+    # Find the 'key' entry, remove the entry with that key and return the deleted entry’s value.
+  end
 end
